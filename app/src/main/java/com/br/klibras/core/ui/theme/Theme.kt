584fdbede24542ebc187c74b34c2b914507793b5
Parallax73 <narakagaara@gmail.com>
2025-10-10 15:36:14 -0300

    revert: Revert Theme.kt to version from 3c9bb07

--- app/src/main/java/com/br/klibras/core/ui/theme/Theme.kt
+++ app/src/main/java/com/br/klibras/core/ui/theme/Theme.kt
@@ -1,43 +1,47 @@
 package com.br.klibras.core.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = HighlightYellow,
+    primary = HighlightYelow,
+    onPrimary = Black100,
+
+    background = Grey100,
+    onBackground = White,
+
+    surface = Black100,
+    onSurface = White,
+
     secondary = Grey70,
     tertiary = Grey50
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = HighlightYellow,
+    primary = HighlightYelow,
+    onPrimary = Black100,
+
+    background = Beige,
+    onBackground = Black100,
+
+    surface = White,
+    onSurface = Black100,
+
     secondary = Grey50,
     tertiary = Grey70
-
 )
 
 @Composable
 fun KLibrasTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
